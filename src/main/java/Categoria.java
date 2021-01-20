a8850961dc50da772d881819c37718c18e0fba43
jgdml <jgdml2801@gmail.com>
2021-01-20 14:46:50 -0300

    Primeira tabela

---
+++ src/main/java/Categoria.java
@@ -0,0 +1,26 @@
+public class Categoria {
+
+    private String nome;
+    private String descricao;
+
+    Categoria(String nome, String descricao){
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
